22537afed07aad4e6fecdfb2a3d8d634e5d72134
Samuele Bianchedi <samuele.bianchedi@studio.unibo.it>
2025-12-17 12:36:26 +0100

    added UseFlowBorder

--- UseFlowBorder.java
+++ UseFlowBorder.java
@@ -2,5 +2,20 @@ import java.awt.*;
 import javax.swing.*;
 
 public class UseFlowBorder {
-    
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame("Flow Border", new BorderLayout());
+
+        final JPanel panelNorth = new JPanel(new FlowLayout());
+        panelNorth.add(new JButton("OK"));
+        panelNorth.add(new JButton("Cancel"));
+
+        final JPanel panelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        panelRight.add(new JButton("Yes"));
+        panelRight.add(new JButton("No"));
+
+        frame.getMainPanel().add(panelNorth, BorderLayout.NORTH);
+        frame.getMainPanel().add(panelRight, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
 }
